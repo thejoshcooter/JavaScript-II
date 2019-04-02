82de8f665ed7133b5e27c194fb6b82fc588f03af
joshcooter <thejoshcooter@gmail.com>
2019-04-02 17:47:58 -0500

    complete closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,19 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function shout(frustration) {
+    const problem = frustration;
+    const name = 'Josh';
+    console.log(`Hi ${name}, how is ${problem} going?`);
+    
+    function answer() {
+        console.log(`Dude, ${problem} is melting my brain!`);
+    }
+    answer();
+}
+
+console.log(shout('JavaScript'));
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,10 +21,17 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+    let count = 0;
+    return () => ++count;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
